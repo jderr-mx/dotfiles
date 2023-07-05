e0496480077364908bea18f4f1764689a8580f33
John Derr <john.derr@allovue.com>
2023-07-05 13:55:07 -0400

    more testing for gitcustom

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ mkdir -p ~/.config/nvim
 ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/nvim/config ~/.config/nvim/config
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/gitcustom/gitcustom ~/.gitcustom
+ln -s ~/gitcustom ~/.gitcustom
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/dotfiles/zsh/zsh_prompt ~/.zsh_prompt
